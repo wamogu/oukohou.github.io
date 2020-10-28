49620f9796311d56905fa0a0f68d69881d01e04b
wamogu <33853987+wamogu@users.noreply.github.com>
2020-10-28 21:05:04 +0800

    修改配置文件

--- assets/js/tongji.baidu.js
+++ assets/js/tongji.baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ec7470452f344e57c8085be5be58583f";
-  var s = document.getElementsByTagName("script")[0];
+  hm.src = "https://hm.baidu.com/hm.js?83c092b7598f0f380ce6eaaeb897edcf";
+  var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
